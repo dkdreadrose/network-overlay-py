be9bae36dcadda375944662ae4fb3af5a4ead41a
christopher.kane <christopher.kane.14.ctr@us.af.mil>
2021-03-24 15:13:01 -0500

    Started initializing

--- src/main/main.py
+++ src/main/main.py
@@ -1,3 +1,5 @@
+import json
+
 import numpy as np
 import pandas as pd
 
@@ -5,11 +7,34 @@ import plotly.offline as py
 import plotly.graph_objects as go
 import networkx as nx
 
+import sys
 
-def main():
+class NetworkMap:
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+
+    def map(self):
+        netmap = nx.Graph()
+
+
+def main(args):
     print("Hello World!")
-    
+    filename = args[1]
+
+    mapfile = open(filename, 'r')
+    map_data = json.load(mapfile)
+    mapfile.close()
+
+    nodes = map_data["network"]["nodes"]
+    connections = map_data["network"]["connections"]
+
+    netmap = NetworkMap(nodes, connections)
+
+
+    print(connections)
+
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
