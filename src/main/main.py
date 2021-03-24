4cbdbb08c1d7aeb5b846de1ee4ccc7940cf094f3
christopher.kane <christopher.kane.14.ctr@us.af.mil>
2021-03-24 16:25:04 -0500

    Added graphing function, still does not save plot

--- src/main/main.py
+++ src/main/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 import plotly.offline as py
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import networkx as nx
 
 import sys
@@ -12,14 +13,21 @@ import sys
 class NetworkMap:
     def __init__(self, nodes, edges):
         self.nodes = nodes
-        self.edges = edges
+        self.edges = []
+        for edge in edges:
+            self.edges.append( (edge[0], edge[1]) )
 
     def map(self):
         netmap = nx.Graph()
+        netmap.add_edges_from(self.edges)
+
+        plt.subplot(121)
+        nx.draw(netmap, with_labels=True, font_weight='bold')
+        plt.savefig("C:\\Users\\BATLab-035\\network-overlay-py\\network_graph.png", format="png")
+        plt.show()
 
 
 def main(args):
-    print("Hello World!")
     filename = args[1]
 
     mapfile = open(filename, 'r')
@@ -31,8 +39,6 @@ def main(args):
 
     netmap = NetworkMap(nodes, connections)
 
-
-    print(connections)
 
 
 
